35e0fcba5d1deab58c9381f2e724a07d06de2951
TimedIn <git@timedin.net>
2026-09-25 18:50:36 +0200

    fix: empty condition fields on restore

--- src/frontend/app/builder/question-selector.ts
+++ src/frontend/app/builder/question-selector.ts
@@ -1,26 +1,31 @@
 import { Component, computed, input, output } from '@angular/core';
-import { MatSelectModule } from '@angular/material/select';
-import { PageDefinition, ElementDefinition, ElementType } from '@shared/model';
 import { FormsModule } from '@angular/forms';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSelectModule } from '@angular/material/select';
+import { ElementDefinition, PageDefinition } from '@shared/model';
 
 @Component({
   selector: 'fm-question-selector',
   templateUrl: './question-selector.html',
-  imports: [FormsModule, MatSelectModule],
+  imports: [FormsModule, MatSelectModule, MatExpansionModule],
 })
 export class QuestionSelector {
   readonly pages = input.required<PageDefinition[]>();
-  readonly model = input.required();
+  readonly model = input.required<string>();
   readonly selfId = input.required<string>();
 
-  readonly selectChange = output<{ id: string; type: ElementType }>();
+  readonly selectChange = output<ElementDefinition>();
+
+  selected(selected: boolean, option: ElementDefinition) {
+    if (selected) {
+      this.selectChange.emit(option);
+    }
+  }
 
   readonly _pages = computed(() => {
     const self = this.selfId();
 
-    function filterFn<T extends ElementDefinition | PageDefinition>(
-      e: T[],
-    ): Omit<T, 'SectionElement'>[] {
+    function filterFn<T extends ElementDefinition | PageDefinition>(e: T[]): T[] {
       return e
         .filter((e) => !(e.id === self || ('type' in e && e.type === 'section')))
         .map((e) => {
